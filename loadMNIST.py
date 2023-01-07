f73c29877f8077bf2a7624a1565d47a9728dc94e
Tal Nesher <tal.nesher@gmail.com>
2023-01-07 11:42:38 +0200

    final project V2

--- loadMNIST.py
+++ loadMNIST.py
@@ -87,7 +87,7 @@ for i in range(0, 5):
 
 #show_images(images_2_show, titles_2_show)
 
-#__________________a_____________________
+#______________________________a______________________________
 
 #reduce all images to between 0.5 to -0.5
 x_train = np.array(x_train)
@@ -96,7 +96,7 @@ x_train = (x_train / 255.0) - 0.5
 x_test = (x_test / 255.0) - 0.5
 m = x_train.shape[0]
 
-#___________________b__________________
+#______________________________b______________________________
 
 #flattening the images
 X = (x_train.reshape(x_train.shape[0], -1)).T
@@ -131,19 +131,20 @@ x_reconstructed = x_reconstructed.reshape(28, 28)
 # plt.imshow(x_train[i], cmap = 'gray')
 # plt.title('Original image')
 # plt.subplot(1, 2, 2)
-# plt.imshow(x_reconstructed, cmap='gray')
+# plt.imshow(x_reconstructed, cmap = 'gray')
 # plt.title('Reconstructed image')
 # plt.show()
 
-#_________________c_______________
-
-def kmeans(X, k, centers_kmeans ,max_iter = 5):
+#______________________________c______________________________
+print("starting c")
+def kmeans(X, k, centers_kmeans ,max_iter = 3):
     for i in range (max_iter):
+        print(i)
         assigned_centers = assign_to_closest_center(X, centers_kmeans)
         centers_kmeans = recompute_centers(X, assigned_centers, k, centers_kmeans)
-        plt.scatter(X[:, 0], X[:, 1], c = assigned_centers, cmap = 'viridis')
-        plt.scatter(centers_kmeans[:, 0], centers_kmeans[:, 1], c = 'red', marker = 'x')
-        plt.show()
+        # plt.scatter(X[:, 0], X[:, 1], c = assigned_centers, cmap = 'viridis')
+        # plt.scatter(centers_kmeans[:, 0], centers_kmeans[:, 1], c = 'red', marker = 'x')
+        # plt.show()
     return assigned_centers,centers_kmeans
 
 
@@ -152,7 +153,7 @@ def assign_to_closest_center(B, centers_assign):
   # Loop through each photo
   for d in range(B.shape[0]):
     # Calculate the distances between the photo and each center
-    photo=B[d]
+    photo = B[d]
     distances = []
     for i in range(centers_assign.shape[0]):
       temp_center = centers_assign[i]
@@ -178,12 +179,12 @@ def recompute_centers(B, assigned_centers, k, centers):
         center = assigned_centers_array[i]
         numOfImagesPerCenter[center] += 1
         for p in range(numOfelements):
-            sumOfVectors[center,p] += B[i,p]
+            sumOfVectors[center, p] += B[i, p]
     for j in range (k):
-        if numOfImagesPerCenter[j]!=0:
+        if numOfImagesPerCenter[j] != 0:
             sumOfVectors[j] = sumOfVectors[j] / numOfImagesPerCenter[j]
         else:
-            sumOfVectors[j]=centers[j]
+            sumOfVectors[j] = centers[j]
     return sumOfVectors
 
 
@@ -194,9 +195,84 @@ def find_max_index(arr):
             max_index = i
     return max_index
 
+#______________________________d______________________________
+print("starting d")
 
-#run kmean with p=40 and randomize centers
+#run kmean with p = 40 and randomize centers
 k = 10
+
+# define each row  to be image, and each coulmn to be element
+w = w.T
+
 # Initialize the centroids randomly
-centroids = np.random.uniform(low=-0.5, high=0.5, size=(k, w.T.shape[1]))
-assigned_centers,centers1 = kmeans(w.T, k, centroids)
+centroids = np.random.uniform(low = -0.5, high = 0.5, size = (k, w.shape[1]))
+assigned_centers, centers1 = kmeans(w, k, centroids)
+assigned_centers = np.array(assigned_centers)
+
+#______________________________e______________________________
+print("starting e")
+
+def give_centers_lebles(assigned_centers, y_train):
+    sums_of_images_per_center = np.zeros((k, k))
+    centers_to_labels = np.zeros(k)
+    for i in range (assigned_centers.shape[0]):
+        image_real_lable = y_train[i]
+        image_assigned_center = assigned_centers[i]
+        sums_of_images_per_center[image_assigned_center, image_real_lable] += 1
+
+    for j in range (k):
+        approximate_center = find_max_index(sums_of_images_per_center[j])
+        centers_to_labels[j] = approximate_center
+    return centers_to_labels
+
+centers_to_labels = give_centers_lebles(assigned_centers, y_train)
+
+#______________________________f______________________________
+print("starting f")
+
+def caculate_succses(centers_to_labels, assigned_centers, y_train, w):
+    succsus = 0
+    for i in range(w.shape[0]):
+        kmeans_center_result = centers_to_labels[assigned_centers[i]]
+        if y_train[i] == kmeans_center_result:
+            succsus += 1
+    return (succsus / w.shape[0]) * 100
+
+succses_presents = caculate_succses(centers_to_labels, assigned_centers, y_train, w)
+print("succses rate is", succses_presents)
+
+#______________________________g______________________________
+print("starting g")
+
+for i in range(3):
+    centroids = np.random.uniform(low = -0.5, high = 0.5, size = (k, w.shape[1])) 
+    assigned_centers, centers1 = kmeans(w, k, centroids)
+    assigned_centers = np.array(assigned_centers)
+    centers_to_labels = give_centers_lebles(assigned_centers, y_train)
+    succses_presents = caculate_succses(centers_to_labels, assigned_centers, y_train, w)
+    print(i, 'has a ',succses_presents, 'precent sucsses rate')
+
+#______________________________i______________________________
+print("starting i")
+
+centroids = np.zeros((10, w.shape[1]))
+for i in range(10):
+        counter = 0
+        index = 0
+        while counter < 10:
+            if(y_train[index] == i):
+                for p in range(w.shape[1]):
+                    centroids[i, p] += w[index, p]
+                counter += 1
+            index += 1
+        centroids[i] = centroids[i] / 10
+
+print('starting kmeans')
+
+assigned_centers, centers1 = kmeans(w, k, centroids)
+assigned_centers = np.array(assigned_centers)
+centers_to_labels = give_centers_lebles(assigned_centers, y_train)
+succses_presents = caculate_succses(centers_to_labels, assigned_centers, y_train, w)
+print('when computing the centers we get ',succses_presents, 'precent sucsses rate')
+
+
